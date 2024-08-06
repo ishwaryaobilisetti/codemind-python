b7adb632735b2e410d4818b12e69e627c954f787
ishwaryaobilisetti <147468419+ishwaryaobilisetti@users.noreply.github.com>
2024-08-06 13:26:16 +0530

    x=int(input()) y=int(input()) print("Sum:{}".format(x+y)) print("Difference:%d"%(x-y)) print("Product:{}".format(x*y)) print("Quotient:{}".format(x//y)) print("Remainder:{}".format(x%y))

---
+++ Arithmetic_operations.py
@@ -0,0 +1,7 @@
+x=int(input())
+y=int(input())
+print("Sum:{}".format(x+y))
+print("Difference:%d"%(x-y))
+print("Product:{}".format(x*y))
+print("Quotient:{}".format(x//y))
+print("Remainder:{}".format(x%y))
